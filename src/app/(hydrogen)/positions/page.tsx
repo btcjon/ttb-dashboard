c717e7bc70416824e8669ec393e105b74060a8f9
Jon Bennett <jonbennett@Jons-MacBook-Pro.local>
2023-11-28 13:16:16 -0500

    aider: Added a basic table component to the `PositionsPage` in `src/app/(hydrogen)/positions/page.tsx`.
    # Aider chat conversation:
    
    ASSISTANT: To add a basic table to the blank page located at `src/app/(hydrogen)/positions/page.tsx`, we will follow the example provided in the `docs/howto_table.md` file. We will create a basic table component and include it in the `page.tsx` file.
    
    Here is the *SEARCH/REPLACE block* to add a basic table to the `page.tsx` file:
    
    src/app/(hydrogen)/positions/page.tsx
    <source>typescript
    <<<<<<< SEARCH
    import PageHeader from '@/app/shared/page-header';
    import { metaObject } from '@/config/site.config';
    
    export const metadata = {
      ...metaObject('Blank Page'),
    };
    
    const pageHeader = {
      title: 'Blank page',
      breadcrumb: [
        {
          href: '/',
          name: 'Home',
        },
        {
          name: 'Blank',
        },
      ],
    };
    
    export default function BlankPage() {
      return (
        <>
          <PageHeader title={pageHeader.title} breadcrumb={pageHeader.breadcrumb} />
        </>
      );
    }
    =======
    import PageHeader from '@/app/shared/page-header';
    import { metaObject } from '@/config/site.config';
    import ControlledTable from '@/components/controlled-table';
    
    export const metadata = {
      ...metaObject('Positions'),
    };
    
    const pageHeader = {
      title: 'Positions',
      breadcrumb: [
        {
          href: '/',
          name: 'Home',
        },
        {
          name: 'Positions',
        },
      ],
    };
    
    // Example data and columns for the table
    const tableData = [
      // Add your table data here
    ];
    const columns = [
      // Define your table columns here
    ];
    
    export default function PositionsPage() {
      return (
        <>
          <PageHeader title={pageHeader.title} breadcrumb={pageHeader.breadcrumb} />
          <ControlledTable
            isLoading={false} // Set to true if data is being loaded
            data={tableData}
            columns={columns}
            className="mt-4"
            // Add any additional props you need for ControlledTable
          />
        </>
      );
    }
    >>>>>>> REPLACE
    </source>
    
    Please note that you will need to define the actual data and columns for the table in the `tableData` and `columns` arrays. The `ControlledTable` component is assumed to be a pre-existing component that you can use to render tables. If it does not exist, you will need to create it or use an alternative table component that you have in your project.

--- src/app/(hydrogen)/positions/page.tsx
+++ src/app/(hydrogen)/positions/page.tsx
@@ -1,27 +1,43 @@
 import PageHeader from '@/app/shared/page-header';
 import { metaObject } from '@/config/site.config';
+import ControlledTable from '@/components/controlled-table';
 
 export const metadata = {
-  ...metaObject('Blank Page'),
+  ...metaObject('Positions'),
 };
 
 const pageHeader = {
-  title: 'Blank page',
+  title: 'Positions',
   breadcrumb: [
     {
       href: '/',
       name: 'Home',
     },
     {
-      name: 'Blank',
+      name: 'Positions',
     },
   ],
 };
 
-export default function BlankPage() {
+// Example data and columns for the table
+const tableData = [
+  // Add your table data here
+];
+const columns = [
+  // Define your table columns here
+];
+
+export default function PositionsPage() {
   return (
     <>
       <PageHeader title={pageHeader.title} breadcrumb={pageHeader.breadcrumb} />
+      <ControlledTable
+        isLoading={false} // Set to true if data is being loaded
+        data={tableData}
+        columns={columns}
+        className="mt-4"
+        // Add any additional props you need for ControlledTable
+      />
     </>
   );
 }
